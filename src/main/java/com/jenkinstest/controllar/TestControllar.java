5488d602c12eedb1f3a099a9f2d93d38d3ef8627
Ankit <ankitt@infusionanalysts.com>
2024-12-30 15:31:34 +0530

    add method

--- src/main/java/com/jenkinstest/controllar/TestControllar.java
+++ src/main/java/com/jenkinstest/controllar/TestControllar.java
@@ -1,7 +1,13 @@
 package com.jenkinstest.controllar;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController("/tests")
 public class TestControllar {
+
+    @GetMapping
+    public  String testString(){
+        return "test";
+    }
 }
